d38bdd9377ba9324e48363c53efe1069ec0fd2a6
kilian florez <134469071+kilejo06@users.noreply.github.com>
2024-11-29 13:20:36 -0500

    Delete util directory

--- util/__dirname.js
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from "url";
-import path, { join } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-//Configuración de directorio raiz carptea SRC
-const __dirname = path.dirname(join(__filename,"../")); 
-console.log(__dirname);
-
-export { __dirname };
